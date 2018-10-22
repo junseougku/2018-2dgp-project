0f90e2a9b8fb2770fd40f81f997352a476402b90
jsg <jsg3001kr@daum.net>
2018-10-23 02:34:58 +0900

    x버튼을 누르면 캐릭터가 반응하게 만듬
    제대로된 점프는 되지않지만
    조금만 수정하면 점프완료

--- mygame.py
+++ mygame.py
@@ -8,26 +8,32 @@ grass_01 = grass.Grass()
 
 def handle_events():
     global running
+    global  jump
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
             close_canvas()
+
         if (event.type ,event.key) == (SDL_KEYDOWN,SDLK_x):
-            playerchar.jump()
+            jump = True
         elif (event.type,event.key) == (SDL_KEYDOWN,SDLK_DOWN):
             pass
 
 running = True
+jump = False
 def run():
+    global running
     while running:
         clear_canvas()
-        handle_events()
         playerchar.update()
 
         grass_01.update()
+        if jump:
+            playerchar.jump()
         grass_01.draw()
         playerchar.draw()
 
         update_canvas()
+        handle_events()
         delay(0.05)
--- player.py
+++ player.py
@@ -2,6 +2,9 @@
 from pico2d import *
 gravity = 0.0
 import math
+
+current_time = get_time()
+
 class Player:
     def __init__(self):
         self.x = 150
@@ -10,13 +13,18 @@ class Player:
         self.frame = 0
 
         self.jumpspeed = 0.0
-        self
     def draw(self):
         self.image.clip_draw(self.frame * 129, 0, 129, 146, self.x, self.y)
        # self.image.clip_draw(self.frame * 160, 0, 160, 152, self.x, self.y)
     def update(self):
         self.frame = (self.frame + 1) % 3
+    def draw_curve(self,curxx,heix,dotx,cury,heiy,doty):
+        for i in range(0,100,2):
+            t = i / 100
+            x = (2*t**2-3*t+1) * curxx + (-4 * t ** 2 + 4 * t) * heix+(2*t**2-t)*doty
+            y = (2*t **2-3*t+1) * cury +(-4 * t ** 2 + 4 * t) * heiy+(2*t**2-t)*doty
+            self.x = x
+            self.y = y
     def jump(self):
-        global gravity
-
+        self.draw_curve(self.x, self.x + 35, self.x + 70, self.y, self.y + 100, self.y)
 
