ce6eeb4b9f261d7e6bdd5917e3f1d70eb72fe8e1
jsg <jsg3001kr@daum.net>
2018-11-19 16:07:17 +0900

    이펙트 효과가 자연스럽게 변경
    이펙트에도 속력을 붙여 자연스럽게 변경 (아이템먹엇을경우)

--- effect.py
+++ effect.py
@@ -1,5 +1,5 @@
 from pico2d import *
-
+import mygame
 class Effect:
     def __init__(self):
         self.image = load_image("image\\eat_effect_.png")
@@ -7,15 +7,17 @@ class Effect:
         self.x = 0
         self.y = 0
         self.active = False
+        self.velocity = mygame.GRASS_SPEED_PPS
     def enter(self,_x,_y):
         self.x = _x
         self.y = _y
         self.active = True
     def draw(self):
         if self.active :
-            self.image.clip_draw(self.frame * 165,0,165,165,self.x,self.y)
+            self.image.clip_draw(self.frame * 181,0,181,165,self.x,self.y)
     def update(self):
         if self.active :
+            mygame.move_update(self)
             self.frame = (self.frame + 1) % 6
             if self.frame == 5:
                 self.active = False
